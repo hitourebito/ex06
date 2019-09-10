a30e0952f98b6d46803269c4b8ac2bdb56e148c0
hitourebito <yuto0423777@gmail.com>
2019-09-10 11:38:25 +0900

    if SERVER["REQUEST_METHOD"] == "POST"

--- rei06.php
+++ rei06.php
@@ -7,6 +7,37 @@
   <meta http-equiv="Expires" content = "0">
   <title>rei06.php</title>
 </head>
+<?php
+  $errmsg[] = array();
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (strlen($_FILES["upfile"]["name"]) <= 0) {
+      $errmsg[] = "ファイルが指定されていません";
+    }
+    else {
+      $filename = $_FILES["upfile"]["name"];
+      $fileinfo = pathinfo($filename);
+      $ext = strtolower($fileinfo["extension"]);
+      if ($ext != "txt" && $ext != "ini") {
+        $errmsg[] = "textかiniのファイルを指定してください";
+      }
+      elseif ($_FILES["upfile"]["size"] == 0) {
+        $errmsg[] = "指定されたファイルが存在しないか空です";
+      }
+      else {
+        $movepath = "img/" . mb_convert_encoding($_FILES["upfile"]["tmp_name"], $movepath);
+        $moveok = move_uploaded_file($_FILES["upfile"]["tmp_name"], $movepath);
+
+        if (!$moveok) {
+          $errmsg[] = "アップロードに失敗しました";
+        }
+      }
+    }
+  }
+  else {
+    $errmsg[] = "正しいリンク元からお越しください";
+  }
+?>
 <body>
-    
+  
 </body>
