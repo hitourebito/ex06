fc7c3eeb680dc082ce4c22eaadd3c808694d7828
hitourebito <yuto0423777@gmail.com>
2019-09-10 12:12:16 +0900

    Copy rei06.php to ex06_01.php

---
+++ ex06_01.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="Pragma" content="no-cache">
+  <meta http-equiv="Cache-Control" content="no-store">
+  <meta http-equiv="Expires" content = "0">
+  <title>rei06.php</title>
+</head>
+<?php
+  $errmsg = array();
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (strlen($_FILES["upfile"]["name"]) <= 0) {
+      $errmsg[] = "ファイルが指定されていません";
+    }
+    else {
+      $filename = $_FILES["upfile"]["name"];
+      $fileinfo = pathinfo($filename);
+      $ext = strtolower($fileinfo["extension"]);
+      if ($ext != "txt" && $ext != "ini") {
+        $errmsg[] = "textかiniのファイルを指定してください";
+      }
+      elseif ($_FILES["upfile"]["size"] == 0) {
+        $errmsg[] = "指定されたファイルが存在しないか空です";
+      } 
+    }
+  }
+  else {
+    $errmsg[] = "正しいリンク元からお越しください";
+  }
+?>
+<body>
+  <div id="err">
+<?php
+  foreach ($errmsg as $val) {
+    echo $val . "<br/>";
+  }
+?>
+  </div>
+<?php
+  if (count($errmsg)) {
+?>
+  <div><br/><a href="rei06.html">アップロード指定に戻る</a></div>
+<?php
+  } else {
+?>
+  アップロード成功！<br/>
+  <?= $filename ?>をアップロードしました
+  <?php } ?>
+</body>
